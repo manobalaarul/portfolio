06ca926a183e836fe69b84f6b87046162fb11e72
Manobala <manobalaarul3@gmail.com>
2025-07-14 23:31:12 +0530

    Nice

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,76 +1,75 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-(function () {
-  "use strict";
 
-  $('.php-email-form').on('submit', function (event) {
-    event.preventDefault();
+  $(document).ready(function () {
+    "use strict";
 
-    let $form = $(this);
-    let action = $form.attr('action');
-    let recaptcha = $form.data('recaptcha-site-key');
+    $('.php-email-form').on('submit', function (event) {
+      event.preventDefault();
 
-    if (!action) {
-      displayError($form, 'The form action property is not set!');
-      return;
-    }
+      let $form = $(this);
+      let action = $form.attr('action');
+      let recaptcha = $form.data('recaptcha-site-key');
 
-    $form.find('.loading').addClass('d-block');
-    $form.find('.error-message').removeClass('d-block');
-    $form.find('.sent-message').removeClass('d-block');
-
-    let formData = new FormData(this);
-
-    if (recaptcha) {
-      if (typeof grecaptcha !== "undefined") {
-        grecaptcha.ready(function () {
-          try {
-            grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
-              .then(function (token) {
-                formData.set('recaptcha-response', token);
-                sendForm($form, action, formData);
-              });
-          } catch (error) {
-            displayError($form, error);
-          }
-        });
-      } else {
-        displayError($form, 'The reCaptcha javascript API url is not loaded!');
+      if (!action) {
+        displayError($form, 'The form action property is not set!');
+        return;
       }
-    } else {
-      sendForm($form, action, formData);
-    }
-  });
 
-  function sendForm($form, action, formData) {
-    $.ajax({
-      url: action,
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest'
-      },
-      success: function (response) {
-        $form.find('.loading').removeClass('d-block');
+      $form.find('.loading').addClass('d-block');
+      $form.find('.error-message').removeClass('d-block');
+      $form.find('.sent-message').removeClass('d-block');
 
-        if (response.trim() === 'OK') {
-          $form.find('.sent-message').addClass('d-block');
-          $form[0].reset();
+      let formData = new FormData(this);
+
+      if (recaptcha) {
+        if (typeof grecaptcha !== "undefined") {
+          grecaptcha.ready(function () {
+            try {
+              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
+                .then(function (token) {
+                  formData.set('recaptcha-response', token);
+                  sendForm($form, action, formData);
+                });
+            } catch (error) {
+              displayError($form, error);
+            }
+          });
         } else {
-          displayError($form, response || 'Form submission failed with no message.');
+          displayError($form, 'The reCaptcha javascript API url is not loaded!');
         }
-      },
-      error: function (xhr, status, error) {
-        $form.find('.loading').removeClass('d-block');
-        displayError($form, `${xhr.status} ${xhr.statusText}`);
+      } else {
+        sendForm($form, action, formData);
       }
     });
-  }
 
-  function displayError($form, error) {
-    $form.find('.loading').removeClass('d-block');
-    $form.find('.error-message').html(error).addClass('d-block');
-  }
-})();
+    function sendForm($form, action, formData) {
+      $.ajax({
+        url: action,
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
+        },
+        success: function (response) {
+          $form.find('.loading').removeClass('d-block');
+          if (response.trim() === 'OK') {
+            $form.find('.sent-message').addClass('d-block');
+            $form[0].reset();
+          } else {
+            displayError($form, response || 'Form submission failed with no message.');
+          }
+        },
+        error: function (xhr) {
+          $form.find('.loading').removeClass('d-block');
+          displayError($form, `${xhr.status} ${xhr.statusText}`);
+        }
+      });
+    }
+
+    function displayError($form, error) {
+      $form.find('.loading').removeClass('d-block');
+      $form.find('.error-message').html(error).addClass('d-block');
+    }
+  });
