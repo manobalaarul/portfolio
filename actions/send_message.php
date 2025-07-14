c66b0e71c896e27e9760573a3be3b3352d316aec
Manobala <manobalaarul3@gmail.com>
2025-07-14 23:38:48 +0530

    New Condition

--- actions/send_message.php
+++ actions/send_message.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["subject"]) && isset($_POST["message"])){
+if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["subject"]) && isset($_POST["message"])) {
 
     $name = $_POST["name"];
     $email = $_POST["email"];
@@ -15,20 +15,34 @@ Email : $email
 Subject : $subject
 Message : $message";
 
-function telegram($msg) {
-        global $telegrambot,$telegramchatid;
-        $url='https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data=array('chat_id'=>$telegramchatid,'text'=>$msg);
-        $options=array('http'=>array('method'=>'POST','header'=>"Content-Type:application/x-www-form-urlencoded\r\n",'content'=>http_build_query($data),),);
-        $context=stream_context_create($options);
-        $result=file_get_contents($url,false,$context);
+    // Set your Bot ID and Chat ID.
+    $telegrambot = '7437847192:AAFm0knUECBG-hALj4jkNoh9TFk7YC_dC8o';
+    $telegramchatid = 7977481924;
+
+    function telegram($msg) {
+        global $telegrambot, $telegramchatid;
+        $url = 'https://api.telegram.org/bot' . $telegrambot . '/sendMessage';
+        $data = array('chat_id' => $telegramchatid, 'text' => $msg);
+        $options = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => "Content-Type:application/x-www-form-urlencoded\r\n",
+                'content' => http_build_query($data),
+            ),
+        );
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
         return $result;
-        }
-                
-        // Set your Bot ID and Chat ID.
-        $telegrambot='7437847192:AAFm0knUECBG-hALj4jkNoh9TFk7YC_dC8o';
-        $telegramchatid= 7977481924;
-        
-        // Function call with your own text or variable
-        telegram ($msg);
     }
-?>
+
+    // Send the message
+    telegram($msg);
+
+    // ✅ Return OK to frontend
+    echo 'OK';
+
+} else {
+    // ❌ Missing POST fields, send error response
+    http_response_code(400);
+    echo 'Missing form fields';
+}
